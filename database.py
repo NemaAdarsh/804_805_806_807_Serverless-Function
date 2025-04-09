dbef7862e9e9ae3ba9a42155d1bb60166b082d01
Adaddy1 <adarshnema6@gmail.com>
2025-04-09 09:00:18 +0530

    dbmore

--- database.py
+++ database.py
@@ -25,3 +25,37 @@ from backend.schemas import FunctionResponse
 from backend.docker_runner import execute_function
 from fastapi import HTTPException
 
+
+def save_function(func: FunctionCreate) -> FunctionResponse:
+    db = SessionLocal()
+    db_func = Function(**func.dict())
+    try:
+        db.add(db_func)
+        db.commit()
+        db.refresh(db_func)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Function with this name already exists")
+    finally:
+        db.close()
+    return FunctionResponse.from_orm(db_func)
+def get_function(func_id: int) -> FunctionResponse:
+    db = SessionLocal()
+    func = db.query(Function).filter(Function.id == func_id).first()
+    db.close()
+    if func is None:
+        raise HTTPException(status_code=404, detail="Function not found")
+    return FunctionResponse.from_orm(func)
+def list_functions() -> List[FunctionResponse]:
+    db = SessionLocal()
+    try:
+        functions = db.query(Function).all()
+        if not functions:
+            raise HTTPException(status_code=404, detail="No functions found")
+        return [FunctionResponse.from_orm(func) for func in functions]
+    except:
+        raise HTTPException(status_code=500, detail="Error retrieving functions")
+    finally:
+        db.close()
+
+

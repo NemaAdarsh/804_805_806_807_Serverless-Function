0973ed451379f41fc6199d4569bbd0f64f556548
Adaddy1 <adarshnema6@gmail.com>
2025-04-09 09:06:30 +0530

    do-not-change-this

---
+++ mainFunctionExecution.py
@@ -0,0 +1,41 @@
+#this file will include the code to execute the functions through http requests
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+
+def create_app():
+    app = FastAPI()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    return app
+
+app = create_app()
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    response = await call_next(request)
+    process_time = response.headers.get("X-Process-Time")
+    if process_time is not None:
+        process_time = float(process_time)
+        if process_time:
+            response.headers["X-Process-Time"] = process_time
+        else:
+            response.headers["X-Process-Time"] = "0"
+        response.headers["X-Function-Execution"] = "Function executed successfully"
+        return response
+    else:
+        response.headers["X-Process-Time"] = "0"
+        response.headers["X-Function-Execution"] = "Function execution failed"
+        return response
